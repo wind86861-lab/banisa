caf2d2878dab681f97e5b2745233734c3d17881c
wind86861-lab <wind86861@gmail.com>
2026-04-06 10:56:56 +0500

    Force new build filename to bust browser cache

--- code/vite.config.js
+++ code/vite.config.js
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name]-[hash]-v2.js',
+            },
+        },
+    },
     server: {
         proxy: {
             '/api': {
